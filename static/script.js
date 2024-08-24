e1f035821928956e64f7a2b46d6d5f0448b77e47
Jason Paek <adventuredonut343@gmail.com>
2024-08-24 00:41:22 -0400

    Home page functionality: add trans, view history, overview added

--- static/script.js
+++ static/script.js
@@ -1,4 +1,11 @@
-//basic js
 document.addEventListener('DOMContentLoaded', function() {
     console.log('Dashboard loaded.');
 });
+
+function showSection(sectionId) {
+    const sections = document.querySelectorAll('.section');
+    sections.forEach(section => {
+        section.style.display = 'none';
+    });
+    document.getElementById(sectionId).style.display = 'block';
+}
